ebd633b10b30cb3f1d3a4862d63e4ea1e5a9a192
Proca Pavel <proka.pavel@gmail.com>
2017-04-09 17:36:33 +0300

    123

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+var flag = 0;
 function next() {
-	document.getElementsByClassName('mnulvl')[1].classList.toggle('active');
 	document.getElementsByClassName('mnulvl')[0].classList.toggle('active');
+	document.getElementsByClassName('mnulvl')[1].classList.toggle('active');
+	flag = 1;
+}
+
+function check() {
+	if (flag == 0) {return;}
+	else if (flag == 1) {
+		flag = 2;
+		document.getElementsByClassName('mnulvl')[0].classList.toggle('move');
+	}
+	else if (flag == 2) {return;}
 }
