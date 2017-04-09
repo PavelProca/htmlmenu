18f8a9184946ad99fd7c8aa8d3865ccbce0baba9
Proca Pavel <proka.pavel@gmail.com>
2017-04-09 17:50:40 +0300

    123

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-var flag = 0;
-function next() {
-	document.getElementsByClassName('mnulvl')[0].classList.toggle('active');
-	document.getElementsByClassName('mnulvl')[1].classList.toggle('active');
-	flag = 1;
+var fcheck = 0;
+var scheck = 0;
+function next(element) {
+	if (fcheck == 0) {
+		document.getElementsByClassName('mnulvl')[0].classList.toggle('active');
+		document.getElementsByClassName('mnulvl')[1].classList.toggle('active');
+		fcheck = 1;
+		scheck = 1;
+	} else if (fcheck == 1) {fcheck=1;}
+	if (element===1) {document.getElementById("p1").innerHTML = "ASD element selected!";}
+	else if (element===2) {document.getElementById("p1").innerHTML = "DSA element selected!";}
+	else if (element===3) {document.getElementById("p1").innerHTML = "QWE element selected!";}
 }
 
 function check() {
-	if (flag == 0) {return;}
-	else if (flag == 1) {
-		flag = 2;
+	if (scheck == 0) {return;}
+	else if (scheck == 1) {
+		scheck = 2;
 		document.getElementsByClassName('mnulvl')[0].classList.toggle('move');
 	}
-	else if (flag == 2) {return;}
+	else if (scheck == 2) {return;}
 }
